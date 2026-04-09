a9a7cb993df3e89c49172bda21ebbe1dd356fc62
Shree Sharma <sharmashree750@gmail.com>
2026-04-09 22:19:39 +0530

    Variables

---
+++ Day1/Variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int myNum;
+    myNum = 15;
+    cout << myNum;
+
+    int x = 5, y = 6, z = 50;
+    cout<< x + y + z;
+
+    int a, b, c;
+    a = b = c = 50;
+    cout << a + b + c;
+}
