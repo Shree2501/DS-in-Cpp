1c8a3d68deafb3705ad2b3725e8ff7171fb3e591
Shree Sharma <sharmashree750@gmail.com>
2026-04-09 22:18:15 +0530

    Comments

---
+++ Day1/Comments.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // This is a comment1
+    cout << "Hello World1!";
+
+    cout << "Hello World2!"; // This is a comment2
+
+    /* The code below will print the words Hello World!
+    to the screen, and it is amazing */
+    cout << "Hello World3!";
+}
