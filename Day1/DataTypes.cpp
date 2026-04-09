01d3a50266a5e51520a2dc46d0a086cab01d6b0f
Shree Sharma <sharmashree750@gmail.com>
2026-04-09 22:18:45 +0530

    Data Types

---
+++ Day1/DataTypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int myNum = 5;               // Integer 
+    cout<<myNum<<endl;
+
+    double myFloatNum = 5.99;    // Floating point number
+    cout<<myFloatNum<<endl;
+
+    char myLetter = 'D';         // Character
+    cout<<myLetter<<endl;
+    
+    string myText = "Hello";     // String 
+    cout<<myText<<endl;
+
+    bool myBoolean = true;       // Boolean (true or false)
+    cout<<myBoolean<<endl;
+}
