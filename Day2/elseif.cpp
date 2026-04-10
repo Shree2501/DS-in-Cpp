edfaef0b629c30b62867908cc38ee7735fe2fb5d
Shree Sharma <sharmashree750@gmail.com>
2026-04-10 13:13:55 +0530

    elseif statement

---
+++ Day2/elseif.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int time = 16;
+    bool isMorning = time < 12;
+    bool isDay = time < 18;
+
+    if (isMorning) {
+        cout << "Good morning.";
+    } 
+    else if (isDay) {
+        cout << "Good day.";
+    } 
+    else {
+        cout << "Good evening.";
+    }
+    
+    return 0;
+}
