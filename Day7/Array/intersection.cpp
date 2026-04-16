8ad2e74ee05c026344a553e0e4f7130a11d5c3b2
Shree Sharma <sharmashree750@gmail.com>
2026-04-16 09:11:46 +0530

    Intersection

---
+++ Day7/Array/intersection.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr1[5] = {6,2,8,4,1};
+    int arr2[5] = {2,3,4,1,5};
+    for(int i=0;i<5;i++){
+        for(int j=0; j<5; j++){
+            if(arr1[i] == arr2[j]){
+                
+            }
+        }
+    }
+}
