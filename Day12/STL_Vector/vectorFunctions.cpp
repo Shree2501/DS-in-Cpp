99db443d80777235bd5e5d3f564e6964b6314cba
Shree Sharma <sharmashree750@gmail.com>
2026-04-23 16:22:32 +0530

    Vector Function

---
+++ Day12/STL_Vector/vectorFunctions.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> vec = {1,2,3};
+    vec.clear();
+    // cout << "Size: " << vec.size() << endl;
+    // cout << "Capacity: " << vec.capacity() << endl;
+
+    // vec.push_back(4);
+    // vec.push_back(5);
+    // cout << "Size after pushback: " << vec.size() << endl;
+    // cout << "Capacity after pushback: " << vec.capacity() << endl;
+    // for(int i : vec){
+    //     cout<<i;
+    // }
+    // cout<<endl;
+
+    // vec.pop_back();
+    // cout << "Size after popback: " << vec.size() << endl;
+    // for(int i : vec){
+    //     cout<<i;
+    // }
+    // cout<<endl;
+
+    // cout << "Front value: " << vec.front() << endl;
+    // cout << "Last value: " << vec.back() << endl;
+    // cout << "Value at index 2: " << vec.at(2);
+
+    // vec.erase(vec.begin() + 1);
+    // vec.insert(vec.begin() + 1, 100);
+    // vec.emplace(vec.begin(), 2);
+    // for(int i : vec){
+    //     cout<< i << " ";
+    // }
+    // cout<<endl;
+
+    // vec.clear();
+    // cout << "Size: " << vec.size() << endl;
+    // cout << "Capacity: " << vec.capacity() << endl;
+    // cout << "Is empty: " << vec.empty() << endl;
+
+}
